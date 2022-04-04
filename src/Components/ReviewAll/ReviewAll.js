e0544df058ed253bfaff76cf9fbedf1d41e7d0c3
kanakweb <kanakwebdev@gmail.com>
2022-04-05 04:46:14 +0600

    added readme text

--- src/Components/ReviewAll/ReviewAll.js
+++ src/Components/ReviewAll/ReviewAll.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 const ReviewProduct = (props) => {
-  const {picture,reName,Rateing,Review, name,} = props.product;
+  const {picture,reName,Rateing,Review} = props.product;
   return (
     <div className="col-xl-4 col-sm-6 mb-5">
-    <div className="bg-white rounded shadow-sm py-5 px-4 w-100"><p className="text-center">
-    <img src={picture} alt="" width="100" className="img-fluid rounded-circle text-centar mb-3 img-thumbnail shadow-sm"/>
-    <h5 className="mb-0">{reName}</h5><span className="small text-uppercase text-muted">{Rateing}</span>
-    <p className="small text-uppercase text-muted py-2">{Review}</p>
-    </p>
-       
-        
-    </div>
-</div>
+            <div className="bg-white rounded shadow-sm py-5 px-4 w-100"><span className="text-center small ">
+            <img src={picture} alt="" width="100" className="img-fluid rounded-circle mx-auto d-block text-centar mb-3 img-thumbnail shadow-sm"/>
+            <h5 className="mb-2">{reName}</h5><h5 className="small text-uppercase text-muted">{Rateing}</h5>
+            <p className="small text-uppercase text-muted py-2">{Review}</p>
+            </span>
+               
+                
+            </div>
+        </div>
   );
 };
 export default ReviewProduct;
